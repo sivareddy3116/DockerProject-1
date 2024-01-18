8fc918f379d226960486b778e115d6d7a8e9d75f
sivareddy3116 <sivareddyboyilla124@gmail.com>
2024-01-18 10:59:35 +0530

    c99

---
+++ WebAppproject/WebAppproject/Controllers/DashBoardController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppproject.Controllers
+{
+    public class DashBoardController : Controller
+    {
+       
+        public IActionResult Index(string username)
+        {
+            ViewBag.Username = username;
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Logout()
+        {
+            // Add logic for logout
+            // Redirect to Login page
+            return RedirectToAction("Login", "Login");
+        }
+    }
+}
---
+++ WebAppproject/WebAppproject/Controllers/LoginController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppproject.Controllers
+{
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(string username, string password)
+        {
+            // Add logic for authentication
+            // Redirect to Dashboard on successful login
+            return RedirectToAction("Index", "Dashboard", new { username = username });
+        }
+
+    }
+}
